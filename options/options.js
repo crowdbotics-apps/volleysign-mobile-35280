d941445c6ffa0a1b13b1d0c51deec0f341f854a9
Crowdbotics <team@crowdbotics.com>
2022-05-19 04:05:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "volleysign_mobile_35280",
+  url: "https://volleysign_mobile_35280.botics.co",
+  api: "https://volleysign_mobile_35280.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
