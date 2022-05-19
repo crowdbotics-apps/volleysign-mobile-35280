0bce03ecb112df0305a2ae19039876d19992953a
Crowdbotics <team@crowdbotics.com>
2022-05-19 04:06:55 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,12 +1,14 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
 
 export const globalOptions = {
   name: "volleysign_mobile_35280",
   url: "https://volleysign_mobile_35280.botics.co",
   api: "https://volleysign_mobile_35280.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
-  }
+    copy: "Routes available!"
+  },
+  undefined: { title: "App Menu", copy: "Routes available!", styles: styles }
 }
